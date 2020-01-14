f08123c3a2d392bdaf46bddfb26da3c455d6b0c0
aniaska4 <aniaska4@wp.pl>
2020-01-14 15:44:09 +0100

    add env files

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/Playlist/'
-      : '/',
+  publicPath: process.env.VUE_APP_PUBLIC_PATH,
 
 
-    devServer: {
-      proxy: {
-        '/api': {
-          target: 'http://omdbapi.com/?i=tt3896198&apikey=14ec1b18',
-          ws: true,
-          changeOrigin: true
-        }
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://omdbapi.com/?i=tt3896198&apikey=14ec1b18',
+        ws: true,
+        changeOrigin: true
       }
     }
   }
+}
